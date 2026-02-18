f2dc7159631c8d012cf9212fba1d80899891a114
rariniello <robert.ariniello@gmail.com>
2026-02-17 18:18:43 -0700

    Updated test camera for new grabbing thread

--- backends/camera_test.py
+++ backends/camera_test.py
@@ -1,9 +1,46 @@
 import time
 
 import numpy as np
-from PyQt6.QtCore import QObject, pyqtSignal, pyqtSlot
+from PyQt6.QtCore import QObject, QThread, pyqtSignal, pyqtSlot
 
 from interfaces import camera_interface
+
+
+class ImageGenerator(QObject):
+    finished = pyqtSignal()
+    imageGrabbedSignal = pyqtSignal(dict)
+
+    def __init__(self, camera):
+        super().__init__()
+        self.camera = camera
+        self.streaming = True
+
+    def run(self):
+        while self.streaming:
+            time.sleep(0.05)
+            x0 = 600 - self.camera._offsetX + int(np.random.normal(0.0, 2.0))
+            y0 = 400 - self.camera._offsetY + int(np.random.normal(0.0, 2.0))
+            sigma = 50
+            img = np.random.randint(
+                0, 5, (self.camera._height, self.camera._width), dtype="int"
+            )
+            x = np.arange(self.camera._width)
+            y = np.arange(self.camera._height)
+            data = (
+                512
+                * np.exp(
+                    -((x[None, :] - x0) ** 2 + (y[:, None] - y0) ** 2) / (2 * sigma**2)
+                )
+                * self.camera._exposure
+            )
+            img += np.int_(data)
+            img *= np.int_(10 ** (self.camera._gain / 10))
+            data = {"image": img}
+            self.imageGrabbedSignal.emit(data)
+        self.finished.emit()
+
+    def stop(self):
+        self.streaming = False
 
 
 class TestCam(QObject):
@@ -14,6 +51,11 @@ class TestCam(QObject):
     height_changed = pyqtSignal(int)
     offsetX_changed = pyqtSignal(int)
     offsetY_changed = pyqtSignal(int)
+    binning_horizontal_changed = pyqtSignal(int)
+    binning_vertical_changed = pyqtSignal(int)
+    image_grabbed = pyqtSignal(dict)
+
+    stop = pyqtSignal()
 
     def __init__(self, serial_number):
         super().__init__()
@@ -23,35 +65,55 @@ class TestCam(QObject):
         self._height = 1024
         self._offsetX = 0
         self._offsetY = 0
+        self._exposureMode = "None"
+        self._triggerMode = "None"
+        self._triggerSource = "None"
+        self._pixelFormat = "Mono 8"
+        self._binning_horizontal = 1
+        self._binning_vertical = 1
 
     def close(self):
         pass
 
     def start_streaming(self):
-        pass
+        self.thread = QThread()
+        self.worker = ImageGenerator(self)
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.run)
+        self.stop.connect(self.worker.stop)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.imageGrabbedSignal.connect(self.emit_image)
+
+        self.thread.start()
 
     def stop_streaming(self):
-        pass
+        self.stop.emit()
 
-    def get_image(self):
-        time.sleep(0.05)
-        x0 = 600 - self._offsetX + int(np.random.normal(0.0, 2.0))
-        y0 = 400 - self._offsetY + int(np.random.normal(0.0, 2.0))
-        sigma = 50
-        img = np.random.randint(0, 5, (self._height, self._width), dtype="int")
-        x = np.arange(self._width)
-        y = np.arange(self._height)
-        data = (
-            512
-            * np.exp(
-                -((x[None, :] - x0) ** 2 + (y[:, None] - y0) ** 2) / (2 * sigma**2)
-            )
-            * self._exposure
-        )
-        img += np.int_(data)
-        img *= np.int_(10 ** (self._gain / 10))
-        return img
+    def emit_image(self, data):
+        self.image_grabbed.emit(data)
 
+    # XXX Should not be used when using the grabbing thread
+    # def get_image(self):
+    #     time.sleep(0.05)
+    #     x0 = 600 - self._offsetX + int(np.random.normal(0.0, 2.0))
+    #     y0 = 400 - self._offsetY + int(np.random.normal(0.0, 2.0))
+    #     sigma = 50
+    #     img = np.random.randint(0, 5, (self._height, self._width), dtype="int")
+    #     x = np.arange(self._width)
+    #     y = np.arange(self._height)
+    #     data = (
+    #         512
+    #         * np.exp(
+    #             -((x[None, :] - x0) ** 2 + (y[:, None] - y0) ** 2) / (2 * sigma**2)
+    #         )
+    #         * self._exposure
+    #     )
+    #     img += np.int_(data)
+    #     img *= np.int_(10 ** (self._gain / 10))
+    #     return img
+
+    # Exposure
+    # -----------------------------------------------------------------
     def set_exposure(self, value):
         self._exposure = value
 
@@ -62,6 +124,8 @@ class TestCam(QObject):
     def get_exposure_range(self):
         return [0.001, 1000.00]
 
+    # Gain
+    # -----------------------------------------------------------------
     def set_gain(self, value):
         self._gain = value
 
@@ -71,6 +135,9 @@ class TestCam(QObject):
 
     def get_gain_range(self):
         return [0.0, 48.0]
+
+    # Width
+    # -----------------------------------------------------------------
 
     def set_width(self, value):
         self._width = value
@@ -82,6 +149,8 @@ class TestCam(QObject):
     def get_width_range(self):
         return [4, 1024]
 
+    # Height
+    # -----------------------------------------------------------------
     def set_height(self, value):
         self._height = value
 
@@ -92,6 +161,8 @@ class TestCam(QObject):
     def get_height_range(self):
         return [4, 1024]
 
+    # Offset X
+    # -----------------------------------------------------------------
     def set_offsetX(self, value):
         self._offsetX = value
         self.offsetX_changed.emit(value)
@@ -103,6 +174,8 @@ class TestCam(QObject):
     def get_offsetX_range(self):
         return [0, 1024 - self._width]
 
+    # Offset Y
+    # -----------------------------------------------------------------
     def set_offsetY(self, value):
         self._offsetY = value
         self.offsetY_changed.emit(value)
@@ -113,3 +186,71 @@ class TestCam(QObject):
 
     def get_offsetY_range(self):
         return [0, 1024 - self._height]
+
+    # Exposure Mode
+    # -----------------------------------------------------------------
+    def set_exposure_mode(self, value):
+        self._exposureMode = value
+
+    def get_exposure_mode(self):
+        return self._exposureMode
+
+    def enumerate_exposure_mode(self):
+        return ["None"]
+
+    # Trigger Mode
+    # -----------------------------------------------------------------
+    def set_trigger_mode(self, value):
+        self._triggerMode = value
+
+    def get_trigger_mode(self):
+        return self._triggerMode
+
+    def enumerate_trigger_mode(self):
+        return ["None"]
+
+    # Trigger Source
+    # -----------------------------------------------------------------
+    def set_trigger_source(self, value):
+        self._triggerSource = value
+
+    def get_trigger_source(self):
+        return self._triggerSource
+
+    def enumerate_trigger_source(self):
+        return ["None"]
+
+    # Pixel Format
+    # -----------------------------------------------------------------
+    def set_pixel_format(self, value):
+        self._pixelFormat = value
+
+    def get_pixel_format(self):
+        return self._pixelFormat
+
+    def enumerate_pixel_format(self):
+        return ["Mono 8"]
+
+    # Binning Horizontal
+    # -----------------------------------------------------------------
+    def set_binning_horizontal(self, value):
+        self._binning_horizontal = value
+        self.binning_horizontal_changed.emit(value)
+
+    def get_binning_horizontal(self):
+        return self._binning_horizontal
+
+    def get_binning_horizontal_range(self):
+        return [0, 4]
+
+    # Binning Vertical
+    # -----------------------------------------------------------------
+    def set_binning_vertical(self, value):
+        self._binning_vertical = value
+        self.binning_vertical_changed.emit(value)
+
+    def get_binning_vertical(self):
+        return self._binning_vertical
+
+    def get_binning_vertical_range(self):
+        return [0, 4]
